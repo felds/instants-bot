f9a60bbbd3ac1634708221f5ea73f958da7c9676
Felds Liscia <dev@felds.com.br>
2020-08-04 21:03:14 -0300

    change types into a module

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,10 @@
-type Instant = {
+import { VoiceConnection } from "discord.js";
+
+export type Instant = {
   title: string;
   url: string;
+};
+
+export type MyChannel = {
+  connection: VoiceConnection;
 };
