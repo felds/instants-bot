624f59fbdb58c82ed390cfc0906a519cdb638c41
Felds Liscia <dev@felds.com.br>
2020-08-08 04:02:33 -0300

    add doorbells

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-import Discord, { Message, VoiceChannel, VoiceConnection } from "discord.js";
+import Discord, {
+  Message,
+  VoiceChannel,
+  VoiceConnection,
+  VoiceState,
+} from "discord.js";
 import config from "./config";
 import CommandHandler from "./src/CommandHandler";
 import Queue from "./src/Queue";
@@ -15,6 +20,57 @@ client.on("ready", () => {
     type: "LISTENING",
   });
 });
+
+client.on(
+  "voiceStateUpdate",
+  async (oldState: VoiceState, newState: VoiceState) => {
+    const voiceChannel = newState.channel;
+    if (!voiceChannel) return;
+
+    const member = newState.member;
+    if (!member || member.user.bot) return;
+
+    const queue = await getQueue(voiceChannel);
+
+    if (member.id === "517135926334324747") {
+      queue.play({
+        url: "https://www.myinstants.com/media/sounds/eaburnea_u4ueT4r.mp3",
+        title: "MIMA entrou na sala",
+      });
+    }
+    if (member.id === "109491752984907776") {
+      queue.play({
+        url: "https://www.myinstants.com/media/sounds/hellou.mp3",
+        title: "FELDS entrou na sala",
+      });
+    }
+    if (member.id === "338896627228213248") {
+      queue.play({
+        url: "https://www.myinstants.com/media/sounds/oh-novinha_mixagem.mp3",
+        title: "YURI entrou na sala",
+      });
+    }
+    if (member.id === "229722390437822464") {
+      queue.play({
+        url: "https://www.myinstants.com/media/sounds/aqui-e-jamal-ok.mp3",
+        title: "JAMAL entrou na sala",
+      });
+    }
+    if (member.id === "692852659765641337") {
+      queue.play({
+        url:
+          "https://www.myinstants.com/media/sounds/manda_o_michael_manuel.mp3",
+        title: "MOTOSO entrou na sala",
+      });
+    }
+    if (member.id === "172161615620341761") {
+      queue.play({
+        url: "https://www.myinstants.com/media/sounds/weeaboo-x-4.mp3",
+        title: "CARRERA entrou na sala",
+      });
+    }
+  }
+);
 
 client.on("message", async (message) => {
   if (message.author.bot) return;
