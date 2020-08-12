26d4d28ba9bf6d8213084097c4036c1601ece2f6
Manoel Motoso <manoel.s.motoso@gmail.com>
2020-08-11 21:44:47 -0300

    fix path to pm2 save

--- scripts/build_and_start.sh
+++ scripts/build_and_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/bot
 npx tsc
-/home/ubuntu/.npm-global/bin/pm2 startOrReload ecosystem.config.js --update-env && pm2 save
+/home/ubuntu/.npm-global/bin/pm2 startOrReload ecosystem.config.js --update-env && /home/ubuntu/.npm-global/bin/pm2 save
