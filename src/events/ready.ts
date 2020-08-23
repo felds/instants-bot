18b540ecc47247d75be42eff5347f4903f636cd2
Felds Liscia <dev@felds.com.br>
2020-08-23 02:20:36 -0300

    minor configs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,12 @@
-import { client } from "../discord";
 import config from "../config";
+import { client } from "../discord";
 import { logger } from "../logging";
 
 client.on("ready", () => {
   logger.info(
     { user: client.user?.tag },
-    "Logged in. Ready to debochar legal."
+    "Logged in. Ready to debochar legal. Listening to prefix %O.",
+    config.prefix,
   );
 
   client.user?.setActivity({
