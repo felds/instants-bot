c83e5fc417257b14ffcadb226fde856a957d6165
Felds Liscia <dev@felds.com.br>
2021-04-15 00:32:34 -0300

    load firebase

--- src/dm/index.ts
+++ src/dm/index.ts
@@ -1,6 +1,34 @@
+import * as admin from "firebase-admin";
+// import "firebase/firestore";
 import { format } from "util";
 import { client } from "../discord";
 import { doorbells } from "../events/doorbell";
+
+admin.initializeApp();
+
+// const fs = admin.firestore();
+// fs.collection("doorbells")
+//   .doc("cleito")
+//   .set({ olha: "a pedra" }, { merge: true })
+//   .then((...x) => {
+//     console.log("x", x);
+//   });
+
+// const firebaseConfig = {
+//   credential: admin.credential.applicationDefault(),
+//   apiKey: "AIzaSyDXwnsu-ujQIJZku1ApL7BFs4lPqVus0M4",
+//   authDomain: "cleiton-bot.firebaseapp.com",
+//   projectId: "cleiton-bot",
+//   storageBucket: "cleiton-bot.appspot.com",
+//   messagingSenderId: "163051923372",
+//   appId: "1:163051923372:web:7de9c6eecf12c28caefc09",
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// const fs = firebase.firestore();
+// // fs.collection("doorbells").doc("teste").
+// const x = fs.doc("doorbells/atacante").set({ olha: "lá" }, { merge: true });
+// console.log("X", x);
 
 client.on("message", (msg) => {
   if (msg.channel.type !== "dm") return;
