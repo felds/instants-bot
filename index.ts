1a7d09650f7c97fff499eb1e8825daf5d6304528
Felds Liscia <dev@felds.com.br>
2021-04-15 17:53:41 -0300

    start server

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
+import http from "http";
 import { join } from "path";
+import util from "util";
 import { importDir } from "./src/util";
 import { loadAppConfig } from "./src/util/firebase";
 
 async function main() {
   await loadAppConfig();
   importDir(join(__dirname, "./src/events"));
+
+  // server
+  const port = process.env.PORT ?? 3000;
+  const server = http.createServer((req, res) => {
+    res.setHeader("Content-type", "text/plain; charset=utf-8");
+    res.write(util.format("\n\nENV: %O", process.env));
+    res.end();
+  });
+  server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 }
 main().catch((err) => `Oops! Something went wrong...\n\n${err}`);
 
@@ -12,14 +25,3 @@ main().catch((err) => `Oops! Something went wrong...\n\n${err}`);
 // import { join } from "path";
 // import "./src/dm";
 // import { importDir } from "./src/util";
-
-// // server
-// const server = http.createServer((req, res) => {
-//   res.setHeader("Content-type", "text/plain; charset=utf-8");
-//   res.write(util.format("\n\nENV: %O", process.env));
-//   res.end();
-// });
-
-// server.listen(process.env.PORT, () => {
-//   console.log(`Listening on port ${process.env.PORT}`);
-// });
