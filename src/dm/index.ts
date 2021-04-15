bf35ac66f09eb52585b9e99dfc229e2d16b644e7
Felds Liscia <dev@felds.com.br>
2021-04-15 03:33:20 -0300

    WIP setting the url from the chat

--- src/dm/index.ts
+++ src/dm/index.ts
@@ -1,23 +1,72 @@
+import {
+  ClientUser,
+  Message,
+  MessageReaction,
+  ReactionCollector,
+  ReactionCollectorOptions,
+} from "discord.js";
 import { format } from "util";
+import { listInstants } from "../connector";
 import { client } from "../discord";
-import { getUserConfig } from "../util/firebase";
+import { Embed } from "../message";
+import { setUserConfig } from "../util/firebase";
+
+const reactionIcons = "1️⃣,2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣".split(",");
 
 client.on("message", async (msg) => {
   if (msg.channel.type !== "dm") return;
   if (msg.author.bot) return;
 
-  const cleanContent = msg.cleanContent;
-  const userId = msg.author.id;
+  const terms = msg.cleanContent;
+  console.log({ terms });
 
-  const userConfig = await getUserConfig(userId);
+  const results = await listInstants(terms, reactionIcons.length);
+  if (results.length < 1) {
+    // myLogger.trace("The search has yielded no results.");
+    msg.reply("Nachei nada, não!");
+    return;
+  }
 
-  msg.reply("reply: " + format(userConfig));
-  msg.author.send("msg: " + format(userConfig));
+  const reactionCollector = await searchResultsEmbed(msg, results, { max: 1 });
 
-  // msg.author.send(
-  //   "Olha essa parada: (userId) " +
-  //     format(cleanContent) +
-  //     format(userId) +
-  //     format(doorbells),
-  // );
+  /** @todo extrair isso aqui */
+  reactionCollector.once("collect", async (reaction, user) => {
+    const emoji = reaction.emoji.name!;
+    const i = reactionIcons.indexOf(emoji);
+    const instant = results[i];
+
+    setUserConfig(msg.author.id, {
+      doorbell: instant.url,
+    });
+
+    msg.reply(format("Pronto. Sua campainha é a `%s`.", instant.title));
+    await reactionCollector.message.delete();
+  });
 });
+
+async function searchResultsEmbed(
+  msg: Message,
+  results: Instant[],
+  config: ReactionCollectorOptions = {},
+): Promise<ReactionCollector> {
+  const desc = results
+    .map((result, i) => `${reactionIcons[i]} ${result.title}`)
+    .join("\n");
+
+  const embed = await msg.channel.send(
+    new Embed({
+      description: desc,
+    }),
+  );
+
+  await Promise.all([
+    reactionIcons.slice(0, results.length).map((r) => embed.react(r)),
+  ]);
+
+  const filter = (reaction: MessageReaction, user: ClientUser) =>
+    reactionIcons.includes(reaction.emoji.name) && !user.bot;
+
+  const collector = embed.createReactionCollector(filter, config);
+
+  return collector;
+}
--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -4,21 +4,19 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 const fs = admin.firestore();
 
-// export type UserConfig = {
-//   doorbell: string|null;
-// }
+export type UserConfig = {
+  doorbell?: string;
+};
 
-export async function getUserConfig(
-  userId: Snowflake,
-): Promise<object | undefined> {
+export async function getUserConfig(userId: Snowflake): Promise<UserConfig> {
   const doc = await fs.doc(`UserConfig/${userId}`).get();
-  return doc.data();
+  return doc.data() || {};
 }
 
 export async function setUserConfig(
   userId: Snowflake,
   config: object,
-): Promise<object | undefined> {
+): Promise<UserConfig> {
   const docId = `UserConfig/${userId}`;
   await fs.doc(docId).set(config, { merge: true });
   const doc = await fs.doc(docId).get();
