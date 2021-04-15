3f69678279a15df7d98058923de63ba79bfa6b78
Felds Liscia <dev@felds.com.br>
2021-04-15 18:32:44 -0300

    add a server so it keeps alive

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
+/// <reference path="./src/types.d.ts" />
+
 import http from "http";
 import { join } from "path";
-import util from "util";
 import { importDir } from "./src/util";
 import { loadAppConfig } from "./src/util/firebase";
 
 async function main() {
   await loadAppConfig();
   importDir(join(__dirname, "./src/events"));
-
-  // server
-  const port = process.env.PORT ?? 3000;
-  const server = http.createServer((req, res) => {
-    res.setHeader("Content-type", "text/plain; charset=utf-8");
-    res.write(util.format("\n\nENV: %O", process.env));
-    res.end();
-  });
-  server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
 }
 main().catch((err) => `Oops! Something went wrong...\n\n${err}`);
+
+// server
+const port = process.env.PORT;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain; charset=utf-8");
+  res.end("Olha a pedra!!");
+});
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 // import http from "http";
 // import { join } from "path";
