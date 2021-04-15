a00dba453ba49d8fa38a735957297cce71f0984d
Felds Liscia <dev@felds.com.br>
2021-04-15 16:59:36 -0300

    add webserver

--- index.ts
+++ index.ts
@@ -7,8 +7,21 @@ Object.entries(require("./.env.json") as { [k: string]: any }).forEach(
   },
 );
 
+import http from "http";
 import { join } from "path";
+import util from "util";
 import "./src/dm";
 import { importDir } from "./src/util";
 
 importDir(join(__dirname, "./src/events"));
+
+// server
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain; charset=utf-8");
+  res.write(util.format("\n\nENV: %O", process.env));
+  res.end();
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
