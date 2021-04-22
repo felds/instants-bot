fa8fbf84739eda8248fec677d39cd986538b9612
Felds Liscia <dev@felds.com.br>
2021-04-22 18:25:57 -0300

    make docker work

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 /// <reference path="./src/types.d.ts" />
 
-import http from "http";
-
 async function main() {
   // await loadAppConfig();
 
@@ -14,18 +12,3 @@ async function main() {
   console.log("Hora do show, porra!");
 }
 main().catch((err) => `Oops! Something went wrong...\n\n${err}`);
-
-// server
-const port = process.env.PORT;
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-type", "text/plain; charset=utf-8");
-  res.end("Olha a pedra!!");
-});
-server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-// import http from "http";
-// import { join } from "path";
-// import "./src/dm";
-// import { importDir } from "./src/util";
