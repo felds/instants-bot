14e211b5549f5558a3115e43bee6182a8e76ef39
Felds Liscia <dev@felds.com.br>
2021-04-22 19:15:12 -0300

    add tenor module

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export default {
   PREFIX: process.env.COMMAND_PREFIX,
   TOKEN: process.env.TOKEN,
   DEBUG: process.env.DEBUG ?? false,
+  TENOR_KEY: process.env.TENOR_KEY,
 };
---
+++ src/util/tenor.ts
@@ -0,0 +1,42 @@
+import assert from "assert";
+import fetch from "node-fetch";
+import querystring from "querystring";
+import config from "../config";
+
+type TenorResponse = {
+  results: Gif[];
+  next: string;
+};
+
+export type Gif = {
+  url: string;
+};
+
+function shuffled<T>(xs: T[]): T[] {
+  const copy = [...xs];
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+  return copy;
+}
+
+export async function searchGifs(
+  q: string,
+  limit: number = 10,
+): Promise<Gif[]> {
+  assert(q !== "", `Q should be a non-empty string.`);
+  assert(
+    limit > 0 && Number.isInteger(limit),
+    `Limit should be a positive integer. (${limit} given)`,
+  );
+
+  const key = config.TENOR_KEY;
+  const qs = querystring.stringify({ q, limit, key });
+
+  const res: TenorResponse = await fetch(
+    `https://g.tenor.com/v1/search?${qs}`,
+  ).then((r) => r.json());
+
+  return shuffled(res.results);
+}
