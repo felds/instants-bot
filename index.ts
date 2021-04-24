38196a94dcdcda125f1c0efe933ca25da0594c0b
Felds Liscia <dev@felds.com.br>
2021-04-23 23:45:38 -0300

    debugging like crazy

--- index.ts
+++ index.ts
@@ -1,10 +1,4 @@
 /// <reference path="./src/types.d.ts" />
 
-async function main() {
-  await Promise.all([
-    // load modules
-    // import("./src/dm"),
-    import("./src/events"),
-  ]);
-}
-main().catch((err) => `Oops! Something went wrong...\n\n${err}`);
+import "./src/dm";
+import "./src/events";
--- src/dm/index.ts
+++ src/dm/index.ts
@@ -13,33 +13,34 @@ import { setUserConfig } from "../util/firebase";
 
 const reactionIcons = "1️⃣,2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣".split(",");
 
-client.on("message", async (msg) => {
-  if (msg.channel.type !== "dm") return;
-  if (msg.author.bot) return;
+client.on("message", async (message) => {
+  if (message.channel.type !== "dm") return;
+  if (message.author.bot) return;
 
-  const terms = msg.cleanContent;
+  const terms = message.cleanContent;
   console.log({ terms });
 
   const results = await listInstants(terms, reactionIcons.length);
   if (results.length < 1) {
     // myLogger.trace("The search has yielded no results.");
-    msg.reply("Nachei nada, não!");
+    message.reply("Nachei nada, não!");
     return;
   }
 
-  const reactionCollector = await searchResultsEmbed(msg, results, { max: 1 });
-
+  const reactionCollector = await searchResultsEmbed(message, results, {
+    max: 1,
+  });
   /** @todo extrair isso aqui */
   reactionCollector.once("collect", async (reaction, user) => {
     const emoji = reaction.emoji.name!;
     const i = reactionIcons.indexOf(emoji);
     const instant = results[i];
 
-    setUserConfig(msg.author.id, {
+    setUserConfig(message.author.id, {
       doorbell: instant.url,
     });
 
-    msg.reply(format("Pronto. Sua campainha é a `%s`.", instant.title));
+    message.reply(format("Pronto. Sua campainha é a `%s`.", instant.title));
     await reactionCollector.message.delete();
   });
 });
@@ -53,20 +54,28 @@ async function searchResultsEmbed(
     .map((result, i) => `${reactionIcons[i]} ${result.title}`)
     .join("\n");
 
-  const embed = await msg.channel.send(
-    new Embed({
-      description: desc,
-    }),
-  );
+  const embed = new Embed({
+    description: desc,
+  });
+  const sentMessage = await msg.channel.send(embed);
 
+  console.log("");
+  console.log("");
+  console.log("");
+  console.log("Entrando no loop do id #" + sentMessage.id);
   for (const r of reactionIcons.slice(0, results.length)) {
-    await embed.react(r);
+    console.log("Reagindo ao id #" + sentMessage.id);
+    sentMessage.react(r).catch((err: Error) => {
+      console.log(
+        "Não foi possível reagir porque " + err.name + " " + sentMessage.id,
+      );
+    });
   }
 
   const filter = (reaction: MessageReaction, user: ClientUser) =>
     reactionIcons.includes(reaction.emoji.name) && !user.bot;
 
-  const collector = embed.createReactionCollector(filter, config);
+  const collector = sentMessage.createReactionCollector(filter, config);
 
   return collector;
 }
