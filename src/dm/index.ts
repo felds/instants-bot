13b6e892563f1f8061d4159fbf0a93333e72aa8e
Felds Liscia <dev@felds.com.br>
2021-04-24 00:45:18 -0300

    refactor out embed function

--- src/dm/index.ts
+++ src/dm/index.ts
@@ -1,68 +1,27 @@
-import {
-  ClientUser,
-  Message,
-  MessageReaction,
-  ReactionCollector,
-  ReactionCollectorOptions,
-} from "discord.js";
 import { format } from "util";
 import { listInstants } from "../connector";
 import { client } from "../discord";
-import { Embed } from "../message";
+import { createSearchResultsEmbed, REACTION_ICONS } from "../message";
 import { setUserConfig } from "../util/firebase";
-
-const reactionIcons = "1️⃣,2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣".split(",");
 
 client.on("message", async (message) => {
   if (message.channel.type !== "dm") return;
   if (message.author.bot) return;
 
   const terms = message.cleanContent;
-  console.log({ terms });
+  const results = await listInstants(terms, REACTION_ICONS.length);
 
-  const results = await listInstants(terms, reactionIcons.length);
   if (results.length < 1) {
     // myLogger.trace("The search has yielded no results.");
     message.reply("Nachei nada, não!");
     return;
   }
 
-  const reactionCollector = await searchResultsEmbed(message, results);
-  reactionCollector.on("collect", async (reaction, user) => {
-    const emoji = reaction.emoji.name!;
-    const i = reactionIcons.indexOf(emoji);
+  await createSearchResultsEmbed(message, results, (i) => {
     const instant = results[i];
-
     setUserConfig(message.author.id, {
       doorbell: instant.url,
     });
-
     message.reply(format("Pronto. Sua campainha é a `%s`.", instant.title));
   });
 });
-
-async function searchResultsEmbed(
-  msg: Message,
-  results: Instant[],
-  config: ReactionCollectorOptions = {},
-): Promise<ReactionCollector> {
-  const desc = results
-    .map((result, i) => `${reactionIcons[i]} ${result.title}`)
-    .join("\n");
-
-  const embed = new Embed({
-    description: desc,
-  });
-  const sentMessage = await msg.channel.send(embed);
-
-  for (const r of reactionIcons.slice(0, results.length)) {
-    sentMessage.react(r).catch(() => {});
-  }
-
-  const filter = (reaction: MessageReaction, user: ClientUser) =>
-    reactionIcons.includes(reaction.emoji.name) && !user.bot;
-
-  const collector = sentMessage.createReactionCollector(filter, config);
-
-  return collector;
-}
--- src/message.ts
+++ src/message.ts
@@ -1,5 +1,38 @@
-import { MessageEmbed, MessageEmbedOptions } from "discord.js";
+import { ClientUser, Message, MessageEmbed, MessageReaction } from "discord.js";
+
+export const REACTION_ICONS = "1️⃣,2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣".split(",");
 
 export class Embed extends MessageEmbed {
   public color = 16562691; // bananana
+}
+
+export async function createSearchResultsEmbed(
+  msg: Message,
+  results: Instant[],
+  onReact?: (i: number) => void,
+): Promise<Message> {
+  const desc = results
+    .map((result, i) => `${REACTION_ICONS[i]} ${result.title}`)
+    .join("\n");
+
+  const embed = new Embed({
+    description: desc,
+  });
+  const sentMessage = await msg.channel.send(embed);
+
+  for (const r of REACTION_ICONS.slice(0, results.length)) {
+    sentMessage.react(r).catch(() => {});
+  }
+
+  const filter = (reaction: MessageReaction, user: ClientUser) =>
+    REACTION_ICONS.includes(reaction.emoji.name) && !user.bot;
+
+  const collector = sentMessage.createReactionCollector(filter);
+  collector.on("collect", (r) => {
+    const emoji = r.emoji.name!;
+    const i = REACTION_ICONS.indexOf(emoji);
+    if (i >= 0) onReact?.(i);
+  });
+
+  return sentMessage;
 }
