cc9924389c35075d87435f7efd73fd84179c5d78
Felds Liscia <dev@felds.com.br>
2021-04-24 00:59:32 -0300

    refactoring the general search

--- src/commands/99-search.ts
+++ src/commands/99-search.ts
@@ -1,11 +1,9 @@
-import { ClientUser, Message, MessageReaction } from "discord.js";
+import { Message } from "discord.js";
 import { listInstants } from "../connector";
 import { logger } from "../logging";
-import { Embed } from "../message";
+import { createSearchResultsEmbed, REACTION_ICONS } from "../message";
 import { Queue } from "../queue";
 import { searchGifs } from "../util/tenor";
-
-const reactionIcons = "1️⃣,2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣".split(",");
 
 export const command: Command = {
   aliases: [],
@@ -22,7 +20,7 @@ export const command: Command = {
 
     myLogger.trace("User made a new search.");
     const [results, gifs] = await Promise.all([
-      listInstants(terms, reactionIcons.length),
+      listInstants(terms, REACTION_ICONS.length),
       searchGifs(terms, 50),
     ]);
 
@@ -32,38 +30,12 @@ export const command: Command = {
       return;
     }
 
-    const desc = results
-      .map((result, i) => `${reactionIcons[i]} ${result.title}`)
-      .join("\n");
-
-    const embed = await message.channel.send(
-      new Embed({
-        description: desc,
-      }),
-    );
-    for (const r of reactionIcons.slice(0, results.length)) {
-      embed.react(r);
-    }
-    const filter = (reaction: MessageReaction, user: ClientUser) =>
-      reactionIcons.includes(reaction.emoji.name) && !user.bot;
-    try {
-      while (true) {
-        const collected = await embed.awaitReactions(filter, {
-          max: 1,
-          time: 300_000,
-        });
-        for (const r of collected.array()) {
-          const emoji = r.emoji.name!;
-          const i = reactionIcons.indexOf(emoji);
-          const instant = results[i];
-          queue.play(instant, () => {
-            const gif = gifs[i];
-            if (gif) message.channel.send(gif.url);
-          });
-        }
-      }
-    } catch (_) {
-      embed.delete();
-    }
+    createSearchResultsEmbed(message, results, (i) => {
+      const instant = results[i];
+      queue.play(instant, () => {
+        const gif = gifs[i];
+        if (gif) message.channel.send(gif.url);
+      });
+    });
   },
 };
