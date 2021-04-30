65ef9f3ae779e94c224d39d9582554319036f07d
Felds Liscia <dev@felds.com.br>
2021-04-30 02:48:22 -0300

    remove lyrics and add gcp config for bunyan

--- src/logging.ts
+++ src/logging.ts
@@ -2,7 +2,16 @@ import bunyan from "bunyan";
 import process from "process";
 import config from "./config";
 
+const level = config.DEBUG ? "trace" : "info";
+
 export const logger = bunyan.createLogger({
   name: "cleiton-bot",
-  streams: [{ level: config.DEBUG ? "trace" : "info", stream: process.stdout }],
+  streams: [{ level, stream: process.stdout }],
 });
+
+if (config.ENV === "gcp") {
+  import("@google-cloud/logging-bunyan").then(({ LoggingBunyan }) => {
+    const connector = new LoggingBunyan();
+    logger.addStream(connector.stream(level));
+  });
+}
