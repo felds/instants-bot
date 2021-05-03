d57e8bcece632379ecce6d4d7759c78c402fefd9
Felds Liscia <dev@felds.com.br>
2021-05-03 18:00:51 -0300

    refactor command runner (WIP)

--- src/events/commands.ts
+++ src/events/commands.ts
@@ -1,31 +1,14 @@
 import assert from "assert";
-import { Message } from "discord.js";
 import { join } from "path";
 import config from "../config";
 import { client } from "../discord";
-import { logger } from "../logging";
-import { getQueue, Queue } from "../queue";
-import { importDir } from "../util";
+import { getQueue } from "../queue";
+import { createCommandRunner } from "../util/command";
 
-export type Command = {
-  aliases: string[];
-  description: string;
-  process: (message: Message, queue: Queue, args: string[]) => Promise<void>;
-};
-
-// import individual commands
-const commandsFolder = join(__dirname, "../commands");
-const commands: Promise<Command[]> = importDir<{ command: Command }>(
-  commandsFolder,
-)
-  .then((modules) => modules.map((module) => module.command))
-  .catch((err) => {
-    logger.fatal("Couldn't load command.", { err });
-    process.exit(1);
-  });
-const defaultCommand = import(join(commandsFolder, "99-search.ts")).then(
-  (x) => x.command,
-) as Promise<Command>;
+const commandRunner = createCommandRunner(
+  join(__dirname, "../commands"),
+  "search",
+);
 
 client.on("message", async (message) => {
   // message from another bot. ignore.
@@ -42,22 +25,9 @@ client.on("message", async (message) => {
   assert(guild, new Error("The message doesn't have a guild."));
 
   const queue = getQueue(guild);
-  let handled = false;
   try {
-    for (const command of await commands) {
-      if (commandRespondsTo(command, args)) {
-        await command.process(message, queue, args);
-        handled = true;
-      }
-    }
-    if (!handled) {
-      await (await defaultCommand).process(message, queue, args);
-    }
+    await commandRunner.then((process) => process(message, queue, args));
   } catch (err) {
     message.reply(err.message);
   }
 });
-
-function commandRespondsTo(command: Command, args: string[]) {
-  return command.aliases.length > 0 && command.aliases.includes(args[0]);
-}
---
+++ src/util/command.ts
@@ -0,0 +1,50 @@
+import { Message } from "discord.js";
+import { Queue } from "../queue";
+import { importDir } from "./fs";
+
+export type ProcessCommandCallback = (
+  message: Message,
+  queue: Queue,
+  args: string[],
+  allCommands?: Command[],
+) => Promise<void>;
+
+export type Command = {
+  name: string;
+  aliases: string[];
+  description: string;
+  process: ProcessCommandCallback;
+};
+
+export async function createCommandRunner(
+  folder: string,
+  defaultCommandName: string,
+): Promise<ProcessCommandCallback> {
+  const commands: Command[] = await importDir<{ command: Command }>(folder)
+    .then((modules) => modules.map((module) => module.command))
+    .catch((err) => {
+      // logger.fatal("Couldn't load command.", { err });
+      console.error("Couldn't load command ", err);
+      process.exit(1);
+    });
+
+  return async (message: Message, queue: Queue, args: string[]) => {
+    // try every handle
+    for (const command of commands) {
+      if (commandRespondsTo(command, args)) {
+        return await command.process(message, queue, args, commands);
+      }
+    }
+
+    // it wasn't handled. try default command.
+    const defaultCommand =
+      defaultCommandName && commands.find((c) => c.name === defaultCommandName);
+    if (defaultCommand) {
+      return await defaultCommand.process(message, queue, args, commands);
+    }
+  };
+}
+
+export function commandRespondsTo(command: Command, args: string[]): boolean {
+  return command.aliases.length > 0 && command.aliases.includes(args[0]);
+}
